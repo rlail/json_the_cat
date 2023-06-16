23515b8116e1476ec10bbe33761744f2246b82ef
Rajan Lail <rajanlail3993@gmail.com>
2023-06-15 18:12:19 -0700

    Refactor. Add module.export

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length > 0) {
-      const firstEntry = data[0];
-      console.log(`${firstEntry.name}: ${firstEntry.description}`);
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(`No results found for the breed "${breedName}".`);
+      const data = JSON.parse(body);
+      if (data.length > 0) {
+        const firstEntry = data[0];
+        callback(null, `${firstEntry.name}: ${firstEntry.description}`);
+      } else {
+        callback(null, `No results found for the breed "${breedName}".`);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
